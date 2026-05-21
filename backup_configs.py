9040495f80b2ef7fee14cbd64f2ea4455bf60ee5
Sameer Aminu <sameeraminuumar521@gmail.com>
2026-05-21 10:47:45 +0100

    added a simlple message to inform that unpushed commits found

--- backup_configs.py
+++ backup_configs.py
@@ -238,6 +238,8 @@ def commit_and_push() -> None:
         state = check_pending_commits()
         if state not in (PENDING, UNPUSHED_DIRTY):
             return
+        else:
+            info("Unpushed commits found.")
 
     ans = input(f"\n{BOLD}Push to remote?{RESET} [y/N] ").strip().lower()
     if ans == "y":
